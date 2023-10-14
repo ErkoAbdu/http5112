9cd6cbc864fdca37b277d4b7b88fd5caf0f05556
Erko Abdurahman <erkoabdu@gmail.com>
2023-10-14 12:24:07 -0400

    added updated assign2 files

--- C#, ASP.NET/Assignments/Assignment 2/Assignment 2/Assignment 2/Controllers/J1Controller.cs
+++ C#, ASP.NET/Assignments/Assignment 2/Assignment 2/Assignment 2/Controllers/J1Controller.cs
@@ -9,6 +9,13 @@ namespace Assignment_2.Controllers
 {
     public class J1Controller : ApiController
     {
+        /// <summary>
+        /// input how many items delivered and how many collisions, 50points for each deliver, -10 for each collision, +500 if delivery > collision
+        /// J1/6/5
+        /// </summary>
+        /// <param name="deliver"></param>
+        /// <param name="collision"></param>
+        /// <returns>750 points</returns>
         //J1 Question Source:https://cemc.math.uwaterloo.ca/contests/computing/past_ccc_contests/2023/ccc/juniorEF.pdf
         [Route("J1/{deliver}/{collision}")]
         [HttpGet]
--- C#, ASP.NET/Assignments/Assignment 2/Assignment 2/Assignment 2/Controllers/J2Controller.cs
+++ C#, ASP.NET/Assignments/Assignment 2/Assignment 2/Assignment 2/Controllers/J2Controller.cs
@@ -9,31 +9,42 @@ namespace Assignment_2.Controllers
 {
     public class J2Controller : ApiController
     {
+        /// <summary>
+        /// input number of sides on dice in dice1 and dice2. If d1 is <= dice1 and 2 increment by 1. If d1 + d2 = 10 increment by 1.
+        /// J2/DiceGame/5/5
+        /// </summary>
+        /// <param name="dice1"></param>
+        /// <param name="dice2"></param>
+        /// <returns>"There is 1 total way to get to the sum of 10"</returns>
         [Route("J2/DiceGame/{dice1}/{dice2}")]
         [HttpGet]
-        public int GetTen(int dice1, int dice2)
+        public string GetTen(int dice1, int dice2)
         {
             int ten = 0;
-            for (dice1 = 1; dice1 < 10; dice1++)
+            string message1 = "There is 1 total way to get to the sum of 10";
+
+            for (int d1 = 1; d1 <= dice1; d1++)
             {
-                for (dice2 = 1; dice2 < 10; dice2++)
+                for (int d2 = 1; d2 <= dice2; d2++)
                 {
-                    if (dice1 + dice2 == 10)
+                    if (d1 + d2 == 10)
                     {
-                        ten ++;
-                    }
+                        ten = ten + 1;
+                    }                        
                 }
             }
-            /*if (ten == 1)
-            {
-                return "There is 1 total way to get the sum of 10";
-            }
-            else
-            {
-                return "There are " + ten + " total ways to get the sum of 10";
-            }*/
-            return ten;
-            
+             if (ten == 1)
+             {
+                 return message1;
+             }
+             else
+             {
+                string message2 = "There are ";
+                string message3 = " total ways to get the sum of 10";
+                string message4 = message2 + ten.ToString() + message3;
+                return message4;
+             }
         }
+
     }
 }
