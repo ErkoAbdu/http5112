594fd8749c0b5c105da0ef8909e7ba8d8754e2ba
Erko Abdurahman <erkoabdu@gmail.com>
2023-10-14 00:18:47 -0400

    assign 2 asp.net

---
+++ C#, ASP.NET/Assignments/Assignment 2/Assignment 2/Assignment 2/Controllers/J1Controller.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Assignment_2.Controllers
+{
+    public class J1Controller : ApiController
+    {
+        //J1 Question Source:https://cemc.math.uwaterloo.ca/contests/computing/past_ccc_contests/2023/ccc/juniorEF.pdf
+        [Route("J1/{deliver}/{collision}")]
+        [HttpGet]
+        public int GetScore(int deliver, int collision)
+        {
+            int delivered = deliver * 50;
+            int collided = collision * -10;
+            int points = (delivered + collided);
+            if (deliver > collision)
+            {
+                return points + 500;
+            }
+            return points;
+        }
+    }
+}
---
+++ C#, ASP.NET/Assignments/Assignment 2/Assignment 2/Assignment 2/Controllers/J2Controller.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Assignment_2.Controllers
+{
+    public class J2Controller : ApiController
+    {
+        [Route("J2/DiceGame/{dice1}/{dice2}")]
+        [HttpGet]
+        public int GetTen(int dice1, int dice2)
+        {
+            int ten = 0;
+            for (dice1 = 1; dice1 < 10; dice1++)
+            {
+                for (dice2 = 1; dice2 < 10; dice2++)
+                {
+                    if (dice1 + dice2 == 10)
+                    {
+                        ten ++;
+                    }
+                }
+            }
+            /*if (ten == 1)
+            {
+                return "There is 1 total way to get the sum of 10";
+            }
+            else
+            {
+                return "There are " + ten + " total ways to get the sum of 10";
+            }*/
+            return ten;
+            
+        }
+    }
+}
